0b1aaab85811588629156d7031231ec2201986e8
Animesh Verma <aniverma10@gmail.com>
2018-10-20 07:46:09 +0530

    Add boilerplate code and related configuration
    Update README
    
    Update package.json

---
+++ config/config.js
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from "path";
+import GitRevSync from "git-rev-sync";
+
+const NODE_ENV = process.env.NODE_ENV;
+const ROOT = process.env.PWD;
+const PATHS = new Map()
+  .set("config", "config")
+  .set("src", "src")
+  .set("dist", "dist")
+  .set("test", "test");
+
+const PACKAGE = JSON.parse(
+  fs.readFileSync("./package.json", { encoding: "utf-8" })
+);
+
+const GIT_INFO = new Map()
+  .set("GIT_VERSION", GitRevSync.count())
+  .set("GIT_DATE", GitRevSync.date())
+  .set("GIT_TAG", GitRevSync.tag())
+  .set("GIT_HASH", GitRevSync.short());
+
+const BANNER = `${PACKAGE.config.title}
+${PACKAGE.config.description}
+Compiled: ${Date()}
+@version v${GitRevSync.count()}
+@link ${PACKAGE.config.url}
+@copyright ${PACKAGE.config.copyright}`;
+
+// https://github.com/ai/browserslist
+const BROWSERS = ["> 1%", "IE 10"];
+
+export { NODE_ENV, ROOT, PATHS, PACKAGE, GIT_INFO, BANNER, BROWSERS };
