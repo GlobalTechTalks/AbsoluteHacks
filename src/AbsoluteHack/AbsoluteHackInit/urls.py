0eabf90271aaafa19252be2f34ea8a77e09b04fd
srihari unnikrishnan <srihari.unnikrishnan@gmail.com>
2018-10-22 17:19:24 +0530

    changed to class based function

--- src/AbsoluteHack/AbsoluteHackInit/urls.py
+++ src/AbsoluteHack/AbsoluteHackInit/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 from . import views
+from django.views.generic import TemplateView
+
 urlpatterns = [
-    path('',views.index)
+    path('',TemplateView.as_view(template_name="AbsoluteHackInit/index.html"))
 ]
