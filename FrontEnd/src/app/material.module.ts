2fc6af72b1fb39ae2e183bba07b968058ceda258
Paul Loh <ploh007@uottawa.ca>
2019-10-10 19:06:01 -0400

    Add angular, angular material and update README instructions

---
+++ FrontEnd/src/app/material.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatGridListModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule
+  ]
+})
+export class MaterialModule { };
